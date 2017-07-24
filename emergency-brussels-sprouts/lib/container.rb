f10abefd00727996a4eae36767ae9603607cf876
Jose Naylor <jose.c.naylor@gmail.com>
2017-07-24 17:38:57 -0400

    finished defining Container class and all its methods

--- emergency-brussels-sprouts/lib/container.rb
+++ emergency-brussels-sprouts/lib/container.rb
@@ -1,5 +1,6 @@
 class Container
-  attr_reader :weight, :maximum_holding_weight, :ingredients
+  attr_reader :weight, :maximum_holding_weight
+  attr_accessor :ingredients
 
   def initialize(weight, maximum_holding_weight)
     @weight = weight
@@ -8,7 +9,14 @@ class Container
   end
 
   def fill_with_ingredient(filler)
-    @ingredients.push(filler)
+    times_can_add = @maximum_holding_weight / filler.weight
+
+    times_can_add.to_i.times do
+      @ingredients.push(filler) 
+      @weight += filler.weight
+    end
+
+    @weight = @weight.round
   end
 
   def which_ingredient
@@ -17,5 +25,15 @@ class Container
 
   def how_many_ingredients
     return @ingredients.size
+  end
+
+  def remove_one_ingredient
+    @ingredients.pop
+    return @ingredients
+  end
+
+  def empty
+    @ingredients = []
+    return @ingredients
   end
 end
