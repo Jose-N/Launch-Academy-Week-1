39087b9a477d47c82e9a8cfd4c498d9e75bf9cce
Jose Naylor <jose.c.naylor@gmail.com>
2017-07-25 12:22:41 -0400

    fully defined test for Card class, started defining test for Deck class

--- blackjack/spec/lib/card_spec.rb
+++ blackjack/spec/lib/card_spec.rb
@@ -1,5 +1,26 @@
 require "spec_helper"
+require_relative "../../lib/card.rb"
 
 RSpec.describe Card do
-  # Your tests here
+  describe "#initialize" do
+    let (:test_card) {Card.new("Heart", "Queen", 10, "Red")}
+
+    context "creates a new card" do
+      it "should have a suit" do
+        expect(test_card.suit).to eq("Heart")
+      end
+
+      it "should have a name" do
+        expect(test_card.name).to eq("Queen")
+      end
+
+      it "should have a value" do
+        expect(test_card.value).to eq(10)
+      end
+
+      it "should have a color" do
+        expect(test_card.color).to eq("Red")
+      end
+    end
+  end
 end
--- blackjack/spec/lib/deck_spec.rb
+++ blackjack/spec/lib/deck_spec.rb
@@ -1,5 +1,28 @@
 require "spec_helper"
 
 RSpec.describe Deck do
-  # Your tests here
+  describe "#initalize" do
+    context "should create a new deck of cards" do
+      it "it should have 52 cards total"
+      it "it should have 26 black cards"
+      it "it should have 26 red cards"
+      it "it should have 13 cards of heart suit"
+      it "it should have 13 cards of spade suit"
+      it "it should have 13 cards of club suit"
+      it "it should have 13 cards of diamond"
+    end
+  end
+
+  describe "#shuffle!" do
+    context "should shuffle the deck" do
+      it "shuffled deck should not equal the original deck"
+    end
+  end
+
+  describe "#deal" do
+    context "should deal a card" do
+      it "it should return the first card in the deck"
+      it "it should remove that card from the deck"
+    end
+  end
 end
