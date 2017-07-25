a966aa4533e311f09fb6acd14659a1e527c4a38d
Jose Naylor <jose.c.naylor@gmail.com>
2017-07-25 14:05:49 -0400

    started defining spec for blackjack

--- blackjack/spec/lib/blackjack_spec.rb
+++ blackjack/spec/lib/blackjack_spec.rb
@@ -1,2 +1,26 @@
 require "spec_helper"
 require_relative "../../lib/blackjack.rb"
+
+RSpec.describe Game do
+  describe "#initialize" do
+    context "should set up a game" do
+      it "it should create a deck"
+      it "it should create a dealer"
+      it "it should create a player"
+      it "it should shuffle the deck"
+    end
+  end
+
+  describe "#play" do
+    context "start game by dealng to cards to each player" do
+      it "player should have two cards"
+      it "dealer should have two cards"
+    end
+
+    context "dealer should hit until points more than 17" do
+    end
+
+    context "should play the game until someone wins" do
+    end
+  end
+end
--- blackjack/spec/lib/card_spec.rb
+++ blackjack/spec/lib/card_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe Card do
     let (:test_card) {Card.new("Heart", "Queen", 10, "Red")}
 
     context "creates a new card" do
-      it "should have a suit" do
+      it "it should have a suit" do
         expect(test_card.suit).to eq("Heart")
       end
 
-      it "should have a name" do
+      it "it should have a name" do
         expect(test_card.name).to eq("Queen")
       end
 
-      it "should have a value" do
+      it "it should have a value" do
         expect(test_card.value).to eq(10)
       end
 
-      it "should have a color" do
+      it "it should have a color" do
         expect(test_card.color).to eq("Red")
       end
     end
