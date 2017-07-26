bdc5969e2d23e8da3f4f8d513c87c65b2ca83810
Jose Naylor <jose.c.naylor@gmail.com>
2017-07-26 07:04:12 -0400

    fixed issuse with wrong number of arguments, was looking at wrong card file. #calculate_hand can now return value of cards in hand and can account for if an Ace should be worth a 1 or 11

--- testing-blackjack-score/lib/hand.rb
+++ testing-blackjack-score/lib/hand.rb
@@ -6,12 +6,34 @@ class Hand
   end
 
   def calculate_hand
-    # Your code here after writing tests
-    # Think about what this method needs to do
+    points = 0 
+    @cards.each do |card|
+      if card.value.is_a? String
+        if card.value === 'A'
+          points += 1
+        else
+          points += 10
+        end
+      else
+        points += card.value
+      end
+    end
+    if points + 10 <= 21 && has_ace?
+      points += 10
+    end
+    return points
+  end
+
+  def has_ace?
+    values = []
+    @cards.each do |card|
+      values << card.value
+    end
+    return values.include?('A')
   end
 end
 
-deck = Deck.new
-cards = deck.deal(2)
-hand = Hand.new(cards)
+#deck = Deck.new
+#cards = deck.deal(2)
+#hand = Hand.new(cards)
 # hand.calculate_hand # Get this working properly
--- testing-blackjack-score/spec/lib/hand_spec.rb
+++ testing-blackjack-score/spec/lib/hand_spec.rb
@@ -5,21 +5,25 @@ describe Hand do
   # '♦', '♣', '♠', '♥'
 
   let(:hand) { Hand.new([Card.new("♦",10), Card.new("♥","J")]) }
+  let(:ace_should_be_one) { Hand.new([Card.new("♦",10), Card.new("♥","J"), Card.new("♥","A")]) }
+  let(:ace_should_be_eleven) { Hand.new([Card.new("♦",10), Card.new("♥","A")]) }
   # You can add more sample hands
 
   describe "#calculate_hand" do
-    # We have included some example tests below. Change these once you get started!
-
-    it "passes" do
-      # Use the RSpec keyword `expect`, as it appears below, to test your assertions
-      expect(1).to eq(1)
+    context "shoud be able to calculate value of hand" do
+      it "it should be able to calculate value of two cards" do
+        expect(hand.calculate_hand).to eq(20)
+      end
     end
 
-    it "fails" do
-      expect(false).to eq true
+    context "should be able to change value of Ace to 1 or 11 depending on value of hand" do
+      it "it should change the value of the Ace to 1 if the value of the hand would be over 21 if Ace was worth 11" do
+        expect(ace_should_be_one.calculate_hand).to eq(21)
+      end
+
+      it "it should change the value of the Ave to 11 if the value of the hand wont go over 21" do
+        expect(ace_should_be_eleven.calculate_hand).to eq(21)
+      end
     end
-
-    # Add your remaining tests here.
-
   end
 end
