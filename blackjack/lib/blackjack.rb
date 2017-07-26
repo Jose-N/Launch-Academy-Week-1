ec63affc6882034f38979e8444bbfbf060533671
Jose Naylor <jose.c.naylor@gmail.com>
2017-07-26 11:48:33 -0400

    broke down hand.rb into more methods and added logic to change the value of aces depending on hand, blackjack.rb also broken down into more methods and logic added to ensure game ends properly

--- blackjack/lib/blackjack.rb
+++ blackjack/lib/blackjack.rb
@@ -8,9 +8,76 @@ class Play
     @player = Hand.new(player)
     @dealer = Hand.new("Dealer")
     @deck.shuffle!
+    @game_over = false
   end
 
   def play
+    stand = false
+    player_round
+    dealer_ai
+    player_round
+    dealer_ai
+
+    while stand == false && @game_over == false
+      puts "Would you like to Hit or Stand (H/S)"
+      if gets.chomp.downcase == 's'
+        stand = true
+      elsif
+        player_round
+        bust
+        dealer_ai
+        bust
+      end
+    end
+
+    while @dealer.points < 17 && @game_over == false
+      dealer_ai
+      bust
+    end
+
+    after_stand
+  end
+
+  def player_round
+    @player.recieve_card(@deck)
+  end
+
+  def dealer_ai
+    if @dealer.points < 17 && @game_over == false
+      @dealer.recieve_card(@deck)
+    else 
+      return false
+    end
+  end
+
+  def check_ace?
+  end
+
+  def bust
+    if @game_over == false
+      if @dealer.points > 21
+        puts "You Win"
+        @game_over = true
+      elsif @player.points > 21
+        puts "You Lose"
+        @game_over = true
+      end
+    end
+  end
+
+  def after_stand
+    if @game_over == false
+      if @player.points > @dealer.points
+        puts "You Win"
+        @game_over = true
+      elsif @player.points < @dealer.points
+        puts "You Lose"
+        @game_over = true
+      elsif @player.points == @player.points
+        puts "You Tied"
+        @game_over = true
+      end
+    end
   end
 end
 
--- blackjack/lib/hand.rb
+++ blackjack/lib/hand.rb
@@ -9,11 +9,43 @@ class Hand
 
   def recieve_card(deck)
     @hand << deck.deal 
+    while check_points > 21 && check_ace?
+      over_21_with_ace
+    end
+    check_points
+
+    puts "#{@name} was dealt a #{@hand[-1].name} of #{@hand[-1].suit}"
+    puts "#{@name} has #{@points} points!"
+    return "#{@name} was dealt a #{@hand[-1].name} of #{@hand[-1].suit}"
+  end
+
+  def check_ace?
+    names_of_cards = []
+    @hand.each do |card| 
+      if card.name == "Ace" && card.value == 11
+        return true
+      else 
+        return false
+      end
+    end
+  end
+
+  def over_21_with_ace
+    if @points > 21 && check_ace?
+      @hand.each do |card|
+        if card.name == "Ace" && card.value == 11
+          card.value = 1
+          break
+        end
+      end
+    end
+  end
+
+  def check_points
     @points = 0
     @hand.each do |card|
       @points += card.value
     end
-    puts "#{@name} was dealt a #{@hand[-1].name} of #{@hand[-1].suit}"
-    return "#{@name} was dealt a #{@hand[-1].name} of #{@hand[-1].suit}"
+    return @points
   end
 end
