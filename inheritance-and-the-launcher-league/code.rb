1c250cd32c917b9d88baff1236d19ce72d5f7878
Jose Naylor <jose.c.naylor@gmail.com>
2017-07-27 13:41:44 -0400

    downloaded launcher leauge exercise

---
+++ inheritance-and-the-launcher-league/code.rb
@@ -0,0 +1,64 @@
+class SuperHero
+  attr_reader :public_identity, :secret_identity
+
+
+  def initialize(public_identity, secret_identity)
+    @public_identity = public_identity
+    @secret_identity = secret_identity
+  end
+
+  def species
+    "Human"
+  end
+
+  def home
+    "Earth"
+  end
+
+  def fans_per_thousand
+    500
+  end
+
+  def powers
+    ""
+  end
+
+  def weakness
+    ""
+  end
+
+  def backstory
+    ""
+  end
+
+  def speed_in_mph
+    60
+  end
+
+  def health
+    100
+  end
+
+  def psychic?
+    false
+  end
+
+end
+
+class Speedster < SuperHero
+end
+
+class Brawler < SuperHero
+end
+
+class Detective < SuperHero
+end
+
+class Demigod < SuperHero
+end
+
+class JackOfAllTrades < SuperHero
+end
+
+class WaterBased < SuperHero
+end
